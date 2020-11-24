5ffc19d1a695b9aa2f0cb2230fdadbbf0f43a5e4
Chanmi Kim <chanmi_dev@naver.com>
2020-11-24 23:41:46 +0900

    feat: Create ex05

--- hello-react/src/ex05.js
+++ hello-react/src/ex05.js
@@ -8,7 +8,14 @@ function App() {
     // undefined를 렌더링하면 에러발생
     // return name;
 
-    return name || '값이 undefined입니다.';
+    // || 사용해서 에러 방지
+    // return name || '값이 undefined입니다.';
+
+    // 내부에서 렌더링 가능
+    // return <div>{name}</div>;
+
+    // name 값이 undefined일 때 보여줄 문구
+    return <div>{name || '리액트'}</div>
 }
 
 export default App;
