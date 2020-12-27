a3fff40327b59640176009c3c842525f618f6493
Chanmi Kim <chanmi_dev@naver.com>
2020-12-27 23:04:17 +0900

    feat: Create ex16 - 3.3.2 컴포넌트를 사용할 때 props 값 지정하기

---
+++ hello-react/src/example/ex16.js
@@ -0,0 +1,13 @@
+/**
+ * App.js
+ * - 3.3.2 컴포넌트를 사용할 때 props 값 지정하기
+ */
+
+import React from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+    return <MyComponent name="React" />;
+};
+
+export default App;
