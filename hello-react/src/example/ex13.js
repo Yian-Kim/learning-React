279f4703a8897f39695f72c238d6475f11214890
Chanmi Kim <chanmi_dev@naver.com>
2020-12-28 22:13:07 +0900

    fix: Move files

--- hello-react/src/example/ex13.js
+++ hello-react/src/example/ex13.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const MyComponent = () => {
-    return <div>나의 새롭고 멋진 컴포넌트</div>
+  return <div>나의 새롭고 멋진 컴포넌트</div>;
 };
 
 /**
  * ES6의 화살표 함수
- * 
+ *
  * 주로 함수를 파라미터로 전달할 때 유용
  */
-setTimeout(function() {
-    console.log('hello world');
+setTimeout(function () {
+  console.log('hello world');
 }, 1000);
 
 setTimeout(() => {
-    console.log('hello world')
-}), 1000);
+  console.log('hello world');
+}, 1000);
 
 /**
  * 기존 function을 대체할 수 없는 이유는 용도가 다름
@@ -23,26 +23,26 @@ setTimeout(() => {
  */
 
 function BlackDog() {
-    this.name = '흰둥이';
-    return {
-        name: '검둥이',
-        bark: function() {
-            console.log(this.name + ': 멍멍!');
-        }
-    }
+  this.name = '흰둥이';
+  return {
+    name: '검둥이',
+    bark: function () {
+      console.log(this.name + ': 멍멍!');
+    },
+  };
 }
 
 const blackDog = new BlackDog();
 blackDog.bark(); // 검둥이: 멍멍!
 
 function WhiteDog() {
-    this.name = '흰둥이';
-    return {
-        name: '검둥이',
-        bark: () => {
-            console.log(this.name + ': 멍멍!');
-        }
-    }
+  this.name = '흰둥이';
+  return {
+    name: '검둥이',
+    bark: () => {
+      console.log(this.name + ': 멍멍!');
+    },
+  };
 }
 
 const whiteDog = new WhiteDog();
@@ -53,13 +53,13 @@ whiteDog.bark(); // 흰둥이: 멍멍!
  * () => 를 사용했을 때는 흰둥이가 나타남.
  * 일반 함수는 자신이 종속된 객체를 this로 가리키며,
  * 화살표 함수는 자신이 종속된 인스턴스를 가리킴.
- * 화살표 함수는 값은 연산하여 바로 반환해야 할 때 
+ * 화살표 함수는 값은 연산하여 바로 반환해야 할 때
  * 사용하면 가독성을 높일 수 있음.
  */
 
 function twice(value) {
-     return value * 2;
- }
+  return value * 2;
+}
 
 const triple = (value) => value * 3;
 
