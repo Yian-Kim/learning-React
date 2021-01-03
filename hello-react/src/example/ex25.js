125b9b7cf4998d6b8bb322e662557f8c0d4d67c0
Chanmi Kim <chanmi_dev@naver.com>
2021-01-03 20:22:23 +0900

    fix: Fix typo in ex25

--- hello-react/src/example/ex25.js
+++ hello-react/src/example/ex25.js
@@ -12,7 +12,7 @@
  this.state.object.value = 5;
 
  // 함수형 컴포넌트에서...
- const [object, setObject] = useState({ a: 1, b: 1});
+ const [object, setObject] = useState({ a: 1, b: 1 });
  object.b = 2;
 
  /**
@@ -22,15 +22,15 @@
   */
 
 // 객체 다루기
-const object = { a: 1, b: 2, c: 3};
-const nextObject = { ...object, b: 2}; // 사본을 만들어서 b 값만 덮어쓰기
+const object = { a: 1, b: 2, c: 3 };
+const nextObject = { ...object, b: 2 }; // 사본을 만들어서 b 값만 덮어쓰기
 
 // 배열 다루기
 const array = {
-    { id: 1, value: true},
-    { id: 2, value: true},
-    { id: 3, value: false}
+    { id: 1, value: true },
+    { id: 2, value: true },
+    { id: 3, value: false }
 };
-let nextArray = array.concat({ id: 4}); // 새 항목 추가
+let nextArray = array.concat({ id: 4 }); // 새 항목 추가
 nextArray.filter(item => item.id != = 2); // id가 2인 항목 제거
 nextArray.map(item => (item.id === 1 ? ( ...item, value: false) : item)); // id가 1인 항목의 value를 false로 설정
