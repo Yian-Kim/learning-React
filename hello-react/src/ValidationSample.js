3df5d807e7080517247f0011798d7cd549ebf61d
Chanmi Kim <chanmi_dev@naver.com>
2021-01-10 23:18:38 +0900

    fix: Update ex29 - 5.2.3.2 버튼 onClick 이벤트 코드 수정

--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -18,12 +18,14 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
     render() {
         return (
             <div>
                 <input
+                    ref={(ref) => this.input=ref}
                     type="password"
                     value={this.state.password}
                     onChange={this.handleChange}
--- hello-react/src/example/ex29.js
+++ hello-react/src/example/ex29.js
@@ -33,12 +33,16 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        // 5.2.3.2 버튼 onClick 이벤트 코드 수정
+        this.input.focus();
     }
     render() {
         return (
             <div>
                 <input
+                    // 5.2.3.1 input에 ref 달기
+                    ref={(ref) => this.input=ref}
                     type="password"
                     value={this.state.password}
                     onChange={this.handleChange}
@@ -69,3 +73,28 @@ class App extends Component {
 }
 
 export default App;
+
+// 5.2 ref 사용
+// 5.2.1 콜백 함수를 통한 ref 설정
+<input ref={(ref) => {this.input=ref}} />
+
+// 5.2.2 createRef를 통한 ref 설정
+import React, { Component } from 'react';
+
+class RefSample extends Component {
+    input = React.createRef();
+    
+    handleFocus = () => {
+        this.input.current.focus();
+    }
+
+    render() {
+        return (
+            <div>
+                <input ref={this.input} />
+            </div>
+        );
+    }
+}
+
+export default RefSample;
