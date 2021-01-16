799a2d401af586c182757e8456a5981de003261f
Chanmi Kim <chanmi_dev@naver.com>
2021-01-16 23:55:57 +0900

    feat: Create ex01 - 8.1.1 useState를 여러 번 사용하기

---
+++ hooks-tutorial/src/example/ex01.js
@@ -0,0 +1,74 @@
+// 8.1 useState
+
+/**
+ * Counter.js
+ */
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [value, setValue] = useState(0);
+
+    return (
+        <div>
+            <p>
+                현재 카운터 값은 <b>{value}</b>입니다.
+            </p>
+            <button onClick={() => setValue(value + 1)}>+1</button>
+            <button onClick={() => setValue(value - 1)}>-1</button>
+        </div>
+    );
+};
+
+export default Counter;
+
+/**
+ * App.js
+ */
+import React from 'react';
+// import Counter from './Counter';
+import Info from './Info';
+
+const App = () => {
+//   return <Counter />;
+    return <Info />;
+};
+
+export default App;
+
+/**
+ * Info.js
+ * - 8.1.1 useState를 여러 번 사용하기
+ */
+import React, { useState } from 'react';
+
+const Info = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName} />
+                <input value={nickname} onChange={onChangeNickname} />
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임:</b> {nickname}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
