45b7c91eff6b1adfe65a22bf3c1ce1321c6198ea
Chanmi Kim <chanmi_dev@naver.com>
2021-01-27 23:50:17 +0900

    feat: Add open-color - 9.2.3 node_modules에서 라이브러리 불러오기

--- styling-react/config/webpack.config.js
+++ styling-react/config/webpack.config.js
@@ -499,20 +499,24 @@ module.exports = function (webpackEnv) {
             {
               test: sassRegex,
               exclude: sassModuleRegex,
-              use: getStyleLoaders(
-                {
-                  importLoaders: 3,
-                  sourceMap: isEnvProduction
-                    ? shouldUseSourceMap
-                    : isEnvDevelopment,
-                },
-                'sass-loader'
-              ),
+              use: getStyleLoaders({
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction && shouldUseSourceMap,
+                }).concat({
+                  loader: require.resolve('sss-loader'),
+                  options: {
+                    sassOptions: {
+                      includePeths: [paths.appSrc + '/styles'],
+                    },
+                    sourceMap: isEnvProduction && shouldUseSourceMap,
+                    prependData: `@import 'utils';`
+                  }
+                }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
               // See https://github.com/webpack/webpack/issues/6571
-              sideEffects: true,
+              sideEffects: true
             },
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
