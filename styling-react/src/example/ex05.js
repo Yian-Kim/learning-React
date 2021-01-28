a1cd9be737f1d3572c8487c3dfca07faba871ea4
Chanmi Kim <chanmi_dev@naver.com>
2021-01-28 23:27:37 +0900

    feat: Create ex05 - 9.3.1 classnames

---
+++ styling-react/src/example/ex05.js
@@ -0,0 +1,10 @@
+// 9.3.1 classnames
+import classNames from 'classnames';
+
+classNames('one', 'two'); // = 'one two'
+classNames('one', { two: true}); // = 'one two'
+classNames('one', { two: false }) // ='one'
+classNames('one', ['two', 'three']); // = 'one two three'
+
+const myClass = 'hello';
+classNames('one', myClass, { myCondition: true }); // = 'one hello myCondition'
