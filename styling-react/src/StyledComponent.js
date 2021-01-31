b9f4e1a60c50691cb54c137c931ecf9f1685727b
Chanmi Kim <chanmi_dev@naver.com>
2021-01-31 23:45:29 +0900

    fix: Update ex06 - 9.4.5 반응형 디자인

--- styling-react/src/StyledComponent.js
+++ styling-react/src/StyledComponent.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import styled, { css } from 'styled-component';
+/* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을
+조건부로 설정해야 하는 경우에는 css를 불러와야 합니다. */
+
+const sizes = {
+    desktop: 1024,
+    tablet: 758
+};
+
+// 위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 줍니다.
+// 참고: https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, lable) => {
+    acc[label] = (...args) => css`
+        @media (max-width: ${sizes[label] / 16}em) {
+            ${css(...args)};
+        }
+    `;
+
+    return acc;
+}, {});
 
 const Box = styled.div`
     /* props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
+    /* 기본적으로는 가로 크기 1024px에 가운데 정렬을 하고
+        가로 크기가 작아짐에 따라 크기를 줄이고
+        768px 미만이 되면 꽉 채웁니다. */
+    width: 1024px;
+    margin: 0 auto;
+    @media (max-width: 1024px) {
+        width: 768px;
+    }
+    @media (max-width: 768px) {
+        width: 100px;
+    }
 `;
 
 const Button = styled.button`
@@ -41,12 +71,3 @@ const Button = styled.button`
         margin-left: 1rem;
     }
 `;
-
-const StyledComponent = () => {
-    <Box color="black">
-        <Button>안녕하세요</Button>
-        <Button inverted={ture}>테두리만</Button>
-    </Box>
-};
-
-export default StyledComponent;
--- styling-react/src/example/ex06.js
+++ styling-react/src/example/ex06.js
@@ -5,11 +5,37 @@
 import React from 'react';
 import styled, { css } from 'styled-component';
 
+// 9.4.5 반응형 디자인
+// 위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 줍니다.
+// 참고: https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, lable) => {
+  acc[label] = (...args) => css`
+      @media (max-width: ${sizes[label] / 16}em) {
+          ${css(...args)};
+      }
+  `;
+
+  return acc;
+}, {});
+
 const Box = styled.div`
     /* props로 넣어 준 값을 직접 전달해 줄 수 있습니다. */
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
+    /* 기본적으로는 가로 크기 1024px에 가운데 정렬을 하고
+      가로 크기가 작아짐에 따라 크기를 줄이고
+      768px 미만이 되면 꽉 채웁니다. */
+    width: 1024px;
+    margin: 0 auto;
+    // @media (max-width: 1024px) {
+    //   width: 768px;
+    // }
+    // @media (max-width: 768px) {
+    //   width: 100%;
+    // }
+    ${media.desktop`width: 769px;`}
+    ${media.tablet`width: 100%;`};
 `;
 
 const Button = styled.button`
