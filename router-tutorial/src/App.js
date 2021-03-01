8956f86a26fba474ce1c0e05591dfd89c80369a9
Chanmi Kim <chanmi_dev@naver.com>
2021-03-01 23:31:46 +0900

    fix: Update ex01 - 13.2.5 Link 컴포넌트를 사용하여 다른 주소로 이동하기

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/About" component={About} />
     </div>
--- router-tutorial/src/example/ex01.js
+++ router-tutorial/src/example/ex01.js
@@ -54,13 +54,22 @@ export default About;
  * App.js
  */
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path="/About" component={About} />
     </div>
@@ -68,3 +77,4 @@ function App() {
 }
 
 export default App;
+
