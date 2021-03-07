2048ab0eba4df5eb7e143543c8552c7cf4a037e9
Chanmi Kim <chanmi_dev@naver.com>
2021-03-07 23:42:34 +0900

    feat: Create ex04 - 13.6.1 history

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 function App() {
   return (
@@ -17,11 +18,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
---
+++ router-tutorial/src/example/ex04.js
@@ -0,0 +1,42 @@
+// 13.6 리액트 라우터 부가 기능
+// 13.6.1 history
+
+/**
+ * HistorySample.js
+ */
+import React, { Component } from 'react';
+
+class HistorySample extends Component {
+    // 뒤로 가기
+    handleGoBack = () => {
+        this.props.history.goBack();
+    };
+
+    // 홈으로 이동
+    handleGoHome = () => {
+        this.props.history.push('/');
+    };
+
+    componentDidMount() {
+        // 이것을 설정하고 나면 페이지에 변화가 생기려고 할 때마다 정말 나갈 것인지를 질문함
+        this.unblock = this.props.history.block('정말 떠나실 건가요?');
+    }
+
+    componentWillUnmout() {
+        // 컴포넌트가 인마운트되면 질문을 멈춤
+        if (this.unblock) {
+            this.unblock();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleGoBack}>뒤로</button>
+                <button onClick={this.handleGoHome}>홈으로</button>
+            </div>
+        );
+    }
+}
+
+export default HistorySample;
