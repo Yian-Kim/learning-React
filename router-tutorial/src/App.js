96f33211d22e92bee581787305987230a22bb303
Chanmi Kim <chanmi_dev@naver.com>
2021-03-07 23:30:17 +0900

    fix: Update ex03 - 13.5 서브 라우트

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -14,16 +15,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      {/* <Route path="/profile/:username" component={Profile} /> */}
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
--- router-tutorial/src/example/ex03.js
+++ router-tutorial/src/example/ex03.js
@@ -27,3 +27,36 @@ const Profiles = () => {
 };
 
 export default Profiles;
+
+/**
+ * App.js
+ */
+ import React from 'react';
+ import { Route, Link } from 'react-router-dom';
+ import About from './About';
+ import Home from './Home';
+ import Profiles from './Profiles';
+ 
+ function App() {
+   return (
+     <div>
+       <ul>
+         <li>
+           <Link to="/">홈</Link>
+         </li>
+         <li>
+           <Link to="/about">소개</Link>
+         </li>
+         <li>
+           <Link to="/profiles">프로필</Link>
+         </li>
+       </ul>
+       <hr />
+       <Route path="/" component={Home} exact={true} />
+       <Route path={['/about', '/info']} component={About} />
+       <Route path="/profiles" component={Profiles} />
+     </div>
+   );
+ };
+ 
+ export default App;
