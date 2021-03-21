3f3831a59129c25bcb580f646aab640fb2503e86
Chanmi Kim <chanmi_dev@naver.com>
2021-03-22 01:38:06 +0900

    feat: Update ex04 - 14.7.3 Categories에서 NavLink 사용하기

--- news-viewer/src/components/Categories.js
+++ news-viewer/src/components/Categories.js
@@ -1,5 +1,6 @@
  import React from 'react';
- import styled, { css } from 'styled-components';
+ import styled from 'styled-components';
+ import { NavLink } from 'react-router-dom';
  
  const categories = [
      {
@@ -43,40 +44,40 @@
      }
  `;
  
-const Category = styled.div`
+const Category = styled(NavLink)`
     font-size: 1.125rem;
     cursor: pointer;
     white-space: pre;
     text-decoration: none;
     color: inherit;
-    padding:bottom: 0.25rem;
+    padding-bottom: 0.25rem;
 
     &.hover {
         color: #495057;
     }
 
-    ${props =>
-        props.active && css`
-            front-weight: 600;
-            border-bottom: 2px solid #22b8cf;
-            color: #22b8cf;
-            &:hover {
-                color: #3bc9db;
-            }
-        `}
+    &.active {
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover {
+            color: #3bc9db;
+        }
+    }
         
     & + & {
         margin-left: 1rem;
     }
 `;
-const Categories = ({ onSelect, category }) => {
+const Categories = () => {
     return (
         <CategoriesBlock>
             {categories.map(c => (
                 <Category
                     key={c.name}
-                    active={cateogry === c.name }
-                    onClick={() => onSelect(c.name)}
+                    activeClassName="active"
+                    exact={c.name === 'all'}
+                    to={c.name === 'all' ? '/' : '/${c.name'}
                 >
                     {c.text}
                 </Category>
