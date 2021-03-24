e32efbb6f9736c66949ddfd782ee2adeeb26fb13
Chanmi Kim <chanmi_dev@naver.com>
2021-03-24 23:49:57 +0900

    feat: update ex01 - 15.2.2 Consumer 사용하기

--- context-tutorial/src/example/ex01.js
+++ context-tutorial/src/example/ex01.js
@@ -3,8 +3,48 @@
 /**
  * contexts/color.js
  */
- import { createContext } from 'react';
+import { createContext } from 'react';
 
- const ColorContext = createContext({ color: 'black'});
- 
- export default ColorContext;
+const ColorContext = createContext({ color: 'black'});
+
+export default ColorContext;
+
+ /**
+  * components/ColorBox.js
+  */
+import React from 'react';
+import ColorContext from '../contexts/color';
+
+const ColorBox = () => {
+    return (
+        <ColorContext.Consumer>
+            {value => {
+                <div
+                    style={{
+                        width: '64px',
+                        height: '64px',
+                        background: value.color
+                    }}
+                />
+            }}
+        </ColorContext.Consumer>
+    );
+};
+
+export default ColorBox;
+
+/**
+ * App.js
+ */
+import React from 'react';
+import ColorBox from './components/ColorBox';
+
+const App = () => {
+    return (
+    <div>
+        <ColorBox />
+    </div>
+    );
+};
+
+export default App;
