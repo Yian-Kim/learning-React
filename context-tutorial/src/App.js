d9fb12befe36a571a0f7db93a3271a0e9dc5ba3f
Chanmi Kim <chanmi_dev@naver.com>
2021-03-27 00:00:33 +0900

    feat: Update ex02 - 15.3.2 새로워진 Context를 프로젝트에 반영하기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
-import ColorContext from './contexts/color';
+import { ColorProvider } from './contexts/color';
 
 const App = () => {
   return (
-    <ColorContext.Provider value={{ color: 'red' }}>
+    <ColorProvider value={{ color: 'red' }}>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 };
 
--- context-tutorial/src/example/ex02.js
+++ context-tutorial/src/example/ex02.js
@@ -33,3 +33,58 @@ const { Consumer: ColorConsumer } = ColorContext;
 export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
+
+/**
+ * App.js
+ * - 15.3.2 새로워진 Context를 프로젝트에 반영하기
+ */
+import React from 'react';
+import ColorBox from './components/ColorBox';
+import { ColorProvider } from './contexts/color';
+
+const App = () => {
+return (
+    <ColorProvider value={{ color: 'red' }}>
+    <div>
+        <ColorBox />
+    </div>
+    </ColorProvider>
+);
+};
+
+export default App;
+
+/**
+ * components/ColorBox.js
+ */
+import React from 'react';
+import { ColorConsumer } from '../contexts/color';
+
+const ColorBox = () => {
+    return (
+        <ColorConsumer>
+            {/* {value => { */}
+            {/* // 15.3.2 새로워진 Context를 프로젝트에 반영하기 */}
+            {({ state }) => (
+                <>
+                    <div
+                        style={{
+                            width: '64px',
+                            height: '64px',
+                            background: state.color
+                        }}
+                    />
+                    <div
+                        style={{
+                            width: '32px',
+                            height: '32px',
+                            background: state.subcolor
+                        }}
+                    />
+                </>
+            )}
+        </ColorConsumer>
+    );
+};
+
+export default ColorBox;
