82df2ccdde1b5a77b9d63eabf95e5e25d3e030ab
Chanmi Kim <chanmi_dev@naver.com>
2021-03-29 23:37:02 +0900

    feat: Update ex02 - 15.3.3 색상 선택 컴포넌트 만들기

--- context-tutorial/src/example/ex02.js
+++ context-tutorial/src/example/ex02.js
@@ -96,6 +96,7 @@ export default ColorBox;
  * components/SelectColors.js
  */
 import React from 'react';
+import { ColorConsumer } from '../contexts/color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']l
 
@@ -103,19 +104,28 @@ const SelectColors = () => {
     return (
         <div>
             <h2>색상을 선택하세요.</h2>
-            <div style={{ display: 'flex' }}>
-                {colors.map(color => (
-                    <div
-                        key={color}
-                        style={{
-                            background: color,
-                            width: '24px',
-                            height: '24px',
-                            cursor: 'pointer'
-                        }}
-                    />
-                ))}
-            </div>
+            {/* // 15.3.3 색상 선택 컴포넌트 만들기 */}
+            <ColorConsumer>
+                {({ actions }) => (
+                    <div style={{ display: 'flex' }}>
+                        {colors.map(color => (
+                            <div
+                                key={color}
+                                style={{
+                                    background: color,
+                                    width: '24px',
+                                    height: '24px',
+                                    cursor: 'pointer' }}
+                                onClick={() => actions.setColor(color)}
+                                onContextMenu={e => {
+                                    e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
+                                    actions.setSubcolor(color);
+                                }}
+                            />
+                        ))}
+                    </div>
+                )}
+            </ColorConsumer>
             <hr />
         </div>
     );
