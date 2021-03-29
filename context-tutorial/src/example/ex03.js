22a1b6d1f37b2ff141dd99e66f78aa5515d8aba3
Chanmi Kim <chanmi_dev@naver.com>
2021-03-29 23:51:51 +0900

    feat: Create ex03 - 15.4.2 static ContextType 사용하기

--- context-tutorial/src/example/ex03.js
+++ context-tutorial/src/example/ex03.js
@@ -1,3 +1,39 @@
+// 15.4 Consumer 대신 Hook 또는 static contextType 사용하기
+// 15.4.1 useContext Hook 사용하기
+/**
+ * components/ColorBox.js
+ */
+import React, { useContext } from 'react';
+import ColorContext from '../contexts/color';
+
+const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+    return (
+        <>
+            <div
+                style={{
+                    width: '64px',
+                    height: '64px',
+                    background: state.color
+                }}
+            />
+            <div
+                style={{
+                    width: '32px',
+                    height: '32px',
+                    background: state.subcolor
+                }}
+            />
+        </>
+    );
+};
+
+export default ColorBox;
+
+// 15.4.2 static ContextType 사용하기
+/**
+ * components/SeletColors.js
+ */
 import React, { Component } from 'react';
 import ColorContext from '../contexts/color';
 
