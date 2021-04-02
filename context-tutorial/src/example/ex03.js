0e16d9f2024ec3a648cfdc8f59b6c8d48ab683aa
Chanmi Kim <chanmi_dev@naver.com>
2021-04-03 01:57:59 +0900

    feat: Update ex03 - 16.2.6 리듀서 함수 정의

--- context-tutorial/src/example/ex03.js
+++ context-tutorial/src/example/ex03.js
@@ -88,3 +88,43 @@ const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
+
+// 1.6.2.4 액션 타입과 액션 생성 함수 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ rtpe: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+// 16.2.5 초기값 설정
+const initialState = {
+    toggle: false,
+    counter: 0
+};
+
+// 16.2.6 리듀서 함수 정의
+//state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+    // action.type에 따라 다른 작업을 처리함
+    switch (action.type) {
+        case TOGGLE_SWTICH:
+            return {
+                ...state, // 불변성 유지를 해 주어야 합니다.
+                toggle: !state.toggle
+            };
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
+}
--- vanilla-redux/index.js
+++ vanilla-redux/index.js
@@ -2,3 +2,40 @@ const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
+
+const TOGGLE_SWITCH = 'TOGGLE_SWITH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increase = difference => ({ rtpe: INCREASE, difference });
+const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+    toggle: false,
+    counter: 0
+};
+
+//state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+    // action.type에 따라 다른 작업을 처리함
+    switch (action.type) {
+        case TOGGLE_SWTICH:
+            return {
+                ...state, // 불변성 유지를 해 주어야 합니다.
+                toggle: !state.toggle
+            };
+        case INCREASE:
+            return {
+                ...state,
+                counter: state.counter + action.difference
+            };
+        case DECREASE:
+            return {
+                ...state,
+                counter: state.counter - 1
+            };
+        default:
+            return state;
+    }
+}
