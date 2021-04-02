72a691ef4090c32d08fe4272b09ccb6e3163eb99
Chanmi Kim <chanmi_dev@naver.com>
2021-04-02 23:55:33 +0900

    fix: Update ex01 - 16.2.2 간단한 UI 구성하기

--- vanilla-redux/example/ex01.js
+++ vanilla-redux/example/ex01.js
@@ -19,3 +19,16 @@
 /**
  * index.html
  */
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="index.css" />
+    </head>
+    <body>
+        <div class="toggle"></div>
+        <hr />
+        <h1>0</h1>
+        <button id="increase">+1</button>
+        <button id="decrease">-1</button>
+        <script src="./index.js"></script>
+    </body>
+</html>
