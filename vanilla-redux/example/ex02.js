535731eb73175dea61a1aa3022007b1d7c5d10c7
Chanmi Kim <chanmi_dev@naver.com>
2021-04-03 02:05:33 +0900

    feat: Update ex02 - 16.2.10 액션 발생시키기

--- vanilla-redux/example/ex02.js
+++ vanilla-redux/example/ex02.js
@@ -1,10 +1,13 @@
-import { createStore } from 'redux';
-
+// 16.2.3 DOM 레퍼런스 만들기
+/**
+ * index.js
+ */
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
+// 1.6.2.4 액션 타입과 액션 생성 함수 정의
 const TOGGLE_SWITCH = 'TOGGLE_SWITH';
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
@@ -13,11 +16,13 @@ const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ rtpe: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
+// 16.2.5 초기값 설정
 const initialState = {
     toggle: false,
     counter: 0
 };
 
+// 16.2.6 리듀서 함수 정의
 //state가 undefined일 때는 initialState를 기본값으로 사용
 function reducer(state = initialState, action) {
     // action.type에 따라 다른 작업을 처리함
@@ -42,8 +47,10 @@ function reducer(state = initialState, action) {
     }
 }
 
+// 16.2.7 스토어 만들기
 const store = createStore(reducer);
 
+// 16.2.8 render 함수 만들기
 const render = () => {
     const state = sotre.getState(); // 현재 상태를 불러옵니다.
     // 토클 처리
@@ -56,9 +63,11 @@ const render = () => {
     counter.innerText = state.counter;
 }
 
-render();
-store.subscribe(render);
 
+render();
+store.subscribe(render); // 16.2.9 구독하기
+
+// 16.2.10 액션 발생시키기
 divToggle.onclick = () => {
     store.dispatch(toggleSwtich());
 };
