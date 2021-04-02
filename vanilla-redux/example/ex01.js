1e4e5d58cc3f6ded700513c49fd2d4d8054956f2
Chanmi Kim <chanmi_dev@naver.com>
2021-04-02 23:53:29 +0900

    feat: Create ex01 - 16.2.2 간단한 UI 구성하기

---
+++ vanilla-redux/example/ex01.js
@@ -0,0 +1,21 @@
+// 16.2. 리액트 없이 쓰는 리덕스
+// 16.2.1 Parcel로 프로젝트 만들기
+// 16.2.2 간단한 UI 구성하기
+/**
+ * index.css
+ */
+ .toggle {
+    border: 2px solid black;
+    width: 64px;
+    height: 64px;
+    border-radius: 32px;
+    box-sizing: border-box;
+}
+
+.toggle.active {
+    background: yellow;
+}
+
+/**
+ * index.html
+ */
