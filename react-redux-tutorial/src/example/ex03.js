00f9ff82cd07efacaa0cbd23c83a0f44e3f230b3
Chanmi Kim <chanmi_dev@naver.com>
2021-04-05 00:00:00 +0900

    feat: Create ex03 - 17.3.1.1 액션 타입 정의하기

---
+++ react-redux-tutorial/src/example/ex03.js
@@ -0,0 +1,8 @@
+// 17.3 리덕스 관련 코드 작성하기
+// 17.3.1 counter 모듈 작성하기
+// 17.3.1.1 액션 타입 정의하기
+/**
+ * modules/counter.js
+ */
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
