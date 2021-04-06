f08c26c03115aac71c822128617bf28d314fe6a9
Chanmi Kim <chanmi_dev@naver.com>
2021-04-06 22:47:11 +0900

    feat: Update ex03 - 17.3.1.3 초기 상태 및 리듀서 함수 만들기

--- react-redux-tutorial/src/example/ex03.js
+++ react-redux-tutorial/src/example/ex03.js
@@ -6,3 +6,29 @@
  */
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+
+// 17.3.1.2 액션 생성 함수 만들기
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 17.3.1.3 초기 상태 및 리듀서 함수 만들기
+const initialState = {
+    number: 0
+};
+
+function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return {
+                number: state.number + 1
+            };
+        case DECREASE:
+            return {
+                number: state.number - 1
+            };
+        default:
+            return state;
+    }
+}
+
+export default counter;
