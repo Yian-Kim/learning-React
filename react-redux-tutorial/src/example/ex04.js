d6a7bde7db74b72fa84081ed734d4c3f6ffdf4e5
Chanmi Kim <chanmi_dev@naver.com>
2021-04-08 00:02:35 +0900

    feat: Update ex04 - 17.3.2.3 초기 상태 및 리듀서 함수 만들기

--- react-redux-tutorial/src/example/ex04.js
+++ react-redux-tutorial/src/example/ex04.js
@@ -33,3 +33,51 @@ export const remove = id => ({
     type: REMOVE,
     id
 });
+
+// 17.3.2.3 초기 상태 및 리듀서 함수 만들기
+const initialState = {
+    input: '',
+    todos: [
+        {
+            id: 1,
+            text: '리덕스 기초 배우기',
+            done: true
+        },
+        {
+            id: 2,
+            text: '리액트와 리덕스 사용하기',
+            done: false
+        }
+    ]
+};
+
+function todos(state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_INPUT:
+            return {
+                ...state,
+                input: action.input
+            };
+        case INSERT:
+            return {
+                ...state,
+                todos: state.todos.concat(action.todo)
+            };
+        case TOGGLE:
+            return {
+                ...state,
+                todos: state.todos.map(todo => 
+                    todo.id = = = action.id ? { ...todo, done: !todo.done } : todo
+                )
+            };
+        case REMOVE:
+            return {
+                ...state,
+                todos: state,todos.filter(todo => todo.id != = action.id)
+            };
+        default:
+            return state;
+    }
+}
+
+export default todos;
