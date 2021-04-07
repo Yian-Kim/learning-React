279c592792423457de2bc2f17f5f6c42d16873ba
Chanmi Kim <chanmi_dev@naver.com>
2021-04-07 23:54:23 +0900

    feat: Create ex04 - 17.3.2.2 액션 생성 함수 만들기

---
+++ react-redux-tutorial/src/example/ex04.js
@@ -0,0 +1,35 @@
+/**
+ * modules/todos.js
+ */
+// 17.3.2.1 액션 타입 정의하기
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋값을 변경함
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; // todo를 제거함
+
+// 17.3.2.2 액션 생성 함수 만들기
+export const changeInput = input => ({
+    type: CHANGE_INPUT,
+    input
+});
+
+let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+
+export const insert = text => ({
+    type: INSERT,
+    todo: {
+        id: id++,
+        text,
+        done: false
+    }
+});
+
+export const toggle = id => ({
+    type: TOGGLE,
+    id
+});
+
+export const remove = id => ({
+    type: REMOVE,
+    id
+});
