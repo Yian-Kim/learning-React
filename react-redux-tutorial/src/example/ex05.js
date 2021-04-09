4496968266f25a98e62b323f3a2df01fae7b8faa
Chanmi Kim <chanmi_dev@naver.com>
2021-04-09 23:41:23 +0900

    feat: Create ex05 - 17.4.2 Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기

--- react-redux-tutorial/src/example/ex05.js
+++ react-redux-tutorial/src/example/ex05.js
@@ -1,3 +1,9 @@
+// 17.4 리액트 애플리케이션에 리덕스 적용하기
+// 17.4.1 스토어 만들기
+// 17.4.2 Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기
+/**
+ * src/index.js
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
