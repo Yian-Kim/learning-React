2d8d3921e0124fe7b6b0f96d022bfe1d8456d8bc
Chanmi Kim <chanmi_dev@naver.com>
2021-04-09 23:31:24 +0900

    feat: Update ex04 - 17.3.3 루트 리듀서 만들기

--- react-redux-tutorial/src/example/ex04.js
+++ react-redux-tutorial/src/example/ex04.js
@@ -81,3 +81,18 @@ function todos(state = initialState, action) {
 }
 
 export default todos;
+
+/**
+ * modules/index.js
+ * - 17.3.3 루트 리듀서 만들기
+ */
+import { combineReducers } from 'redux';
+import counter from './counter';
+import todos from './todos';
+
+const rootReducer = combineReducers({
+    counter,
+    todos
+});
+
+export deafult rootReducer;
