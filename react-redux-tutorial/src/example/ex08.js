c4c6efb7f5ca5fc02883c08ad5ecaa893ac30428
Chanmi Kim <chanmi_dev@naver.com>
2021-04-23 23:58:03 +0900

    feat: Create ex08 - 17.7.1 useSelector로 상태 조회하기

--- react-redux-tutorial/src/example/ex08.js
+++ react-redux-tutorial/src/example/ex08.js
@@ -1,3 +1,8 @@
+// 17.7 Hooks를 사용하여 컨테이너 컴포넌트 만들기
+// 17.7.1 useSelector로 상태 조회하기
+/**
+ * containers/CounterContiner.js
+ */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Counter from '../components/Counter';
