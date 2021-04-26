ea137020b93f49f994ea10290e47bc517f1e77a8
Chanmi Kim <chanmi_dev@naver.com>
2021-04-26 23:55:24 +0900

    feat: Update ex08 - 17.7.2 useDispatch를 사용하여 액션 디스패치하기

--- react-redux-tutorial/src/example/ex08.js
+++ react-redux-tutorial/src/example/ex08.js
@@ -1,16 +1,22 @@
 // 17.7 Hooks를 사용하여 컨테이너 컴포넌트 만들기
 // 17.7.1 useSelector로 상태 조회하기
+// 17.7.2 useDispatch를 사용하여 액션 디스패치하기
 /**
  * containers/CounterContiner.js
  */
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter';
-
-const CounterContainer = () => {
-    const number = useSelector(state => state.counter.number);
-    return <Counter number={number} />;
-};
-
-export default CounterContainer;
+ import React, { useCallback } from 'react';
+ import { useSelector, useDispatch } from 'react-redux';
+ import Counter from '../components/Counter';
+ import { increase, decrease } from '../modules/counter';
+ 
+ const CounterContainer = () => {
+     const number = useSelector(state => state.counter.number);
+     const dispatch = useDispatch();
+     const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+     const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+     return (
+         <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+     );
+ };
+ 
+ export default CounterContainer;
