54f2b0038f1715decce7d64f462d18e78596c38b
Chanmi Kim <chanmi_dev@naver.com>
2021-04-27 23:58:09 +0900

    feat: Update ex08 - 17.7.4 TodosContainer를 Hooks로 전환하기

--- react-redux-tutorial/src/example/ex08.js
+++ react-redux-tutorial/src/example/ex08.js
@@ -20,3 +20,39 @@
  };
  
  export default CounterContainer;
+
+ // 17.7.4 TodosContainer를 Hooks로 전환하기
+ /**
+  * container/TodosContainer.js
+  */
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../components/Todos';
+
+const TodosContainer = () => {
+    const { input, todos } = useSelector(({ todos }) => ({
+        input: todos.input,
+        todos: todos.todos
+    }));
+    const dispatch = useDispatch();
+    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
+        dispatch
+    ]);
+    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+
+    return (
+        <Todos
+            input={input}
+            todos={todos}
+            onChangeInput={changeInput}
+            onInsert={insert}
+            onToggle={toggle}
+            onRemove={remove}
+        />
+    );
+};
+
+export default TodosContainer;
