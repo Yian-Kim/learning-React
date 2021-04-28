bc1d6ad8eb910892e03d6b6ab2ea8eb858315279
Chanmi Kim <chanmi_dev@naver.com>
2021-04-28 23:47:14 +0900

    feat: Update ex08 - 17.7.6 connect 함수와의 주요 차이점

--- react-redux-tutorial/src/example/ex08.js
+++ react-redux-tutorial/src/example/ex08.js
@@ -25,34 +25,64 @@
  /**
   * container/TodosContainer.js
   */
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+// import React, { useCallback } from 'react';
+import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
 const TodosContainer = () => {
     const { input, todos } = useSelector(({ todos }) => ({
         input: todos.input,
         todos: todos.todos
     }));
-    const dispatch = useDispatch();
-    const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
-        dispatch
-    ]);
-    const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
-    const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
-    const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
-
+    // const dispatch = useDispatch();
+    // const onChangeInput = useCallback(input => dispatch(changeInput(input)), [
+    //     dispatch
+    // ]);
+    // const onInsert = useCallback(text => dispatch(insert(text)), [dispatch]);
+    // const onToggle = useCallback(id => dispatch(toggle(id)), [dispatch]);
+    // const onRemove = useCallback(id => dispatch(remove(id)), [dispatch]);
+    // 17.7.5 useActions 유틸 Hook을 만들어서 사용하기
+    const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+        [changeInput, insert, toggle, remove],
+        []
+    );
     return (
         <Todos
             input={input}
             todos={todos}
-            onChangeInput={changeInput}
-            onInsert={insert}
-            onToggle={toggle}
-            onRemove={remove}
+            onChangeInput={onChangeInput}
+            onInsert={onInsert}
+            onToggle={onToggle}
+            onRemove={onRemove}
         />
     );
 };
 
-export default TodosContainer;
+// export default TodosContainer;
+// 17.7.6 connect 함수와의 주요 차이점
+export default React.memo(TodosContainer);
+
+// 17.7.5 useActions 유틸 Hook을 만들어서 사용하기
+/**
+ * lib/useActions.js
+ */
+import { bindActionCreators } from 'redux';
+import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+
+export default function useActions(actions, deps) {
+    const dispatch = useDispatch();
+    return useMemo(
+        () => {
+            if (Array.isArray(actions)) {
+                return actions.map(a => bindActionCreators(a, dispatch));
+            }
+            return bindActionCreators(actions, dispatch);
+        },
+        deps ? [dispatch, ...deps] : deps
+    );
+}
