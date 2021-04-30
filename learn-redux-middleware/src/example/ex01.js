0afc0d45175ff1aaed6668b52f5a3e81a275c59f
Chanmi Kim <chanmi_dev@naver.com>
2021-04-30 23:25:33 +0900

    feat: Create ex01 - 18.1 작업 환경 준비

---
+++ learn-redux-middleware/src/example/ex01.js
@@ -0,0 +1,23 @@
+// 18.1 작업 환경 준비
+/**
+ * modules/counter.js
+ */
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
+
+function counter = handleActions(
+    {
+        [INCREASE]: state => state + 1,
+        [DECREASE]: state => state - 1,
+    },
+    initialState,
+);
+
+export default counter;
