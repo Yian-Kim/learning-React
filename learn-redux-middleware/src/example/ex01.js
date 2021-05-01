b494c86bbb0da9d2dcb92a9794923aa57a2f9ac4
Chanmi Kim <chanmi_dev@naver.com>
2021-05-01 23:29:55 +0900

    feat: Update ex01 - 18.1 작업 환경 준비

--- learn-redux-middleware/src/example/ex01.js
+++ learn-redux-middleware/src/example/ex01.js
@@ -21,3 +21,23 @@ function counter = handleActions(
 );
 
 export default counter;
+
+/**
+ * index.js
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+
+const stroe = createStore(rootReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+document.getElementsById('root')
+);
