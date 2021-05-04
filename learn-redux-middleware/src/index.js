4b0b44bacbdbc66a3bbd18d27d1b419e36a2e2c8
Chanmi Kim <chanmi_dev@naver.com>
2021-05-04 23:29:49 +0900

    feat: Update ex02 - 18.2.2 redux-logger 사용하기

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -5,8 +5,10 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
 
+const logger = createLogger();
 const stroe = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
