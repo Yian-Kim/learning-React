6090e9d18b366f3f15ea3e6267596cd690cc9949
Chanmi Kim <chanmi_dev@naver.com>
2021-05-05 23:34:21 +0900

    feat: Update ex02 - 18.3.1.3 Thunk 생성 함수 만들기

--- learn-redux-middleware/src/example/ex02.js
+++ learn-redux-middleware/src/example/ex02.js
@@ -36,3 +36,38 @@ ReactDOM.render(
 </Provider>
 document.getElementsById('root')
 );
+
+/**
+ * modules/counter.js
+ */
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
+export const increaseAsyncd = () => dispatch => {
+    setTimeout(() => {
+        dispatch(increase());
+    }, 1000);
+    export const decraseAsyncd = () => dispatch => {
+        setTimeout(() => {
+            dispatch(decrease());
+        }, 1000);
+    }
+};
+
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
+
+function counter = handleActions(
+    {
+        [INCREASE]: state => state + 1,
+        [DECREASE]: state => state - 1,
+    },
+    initialState,
+);
+
+export default counter;
