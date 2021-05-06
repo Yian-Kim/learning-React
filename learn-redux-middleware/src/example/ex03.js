b81806551643ca85a104d9e89638c03df9b4280c
Chanmi Kim <chanmi_dev@naver.com>
2021-05-06 23:36:58 +0900

    feat: Update ex03 - 18.3.1.5 리팩토링

--- learn-redux-middleware/src/example/ex03.js
+++ learn-redux-middleware/src/example/ex03.js
@@ -15,6 +15,8 @@ export const getUsers = id =>
  */
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+// 18.3.1.5 리팩토링
+import createRequestThunk from '../lib/createRequestThunk';
 
 // 액션 타입을 선언합니다.
 // 한 요청당 세 개를 만들어야 합니다.
@@ -30,41 +32,44 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 // thunk 함수를 생성합니다.
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치합니다.
 
-export const getPost = id => async dispatch => {
-    dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
-    try {
-        const response = await api.getPost(id);
-        dispatch({
-            type: GET_POST_SUCCESS,
-            payload: response.data
-        }); // 요청 성공
-    } catch (e) {
-        dispatch({
-            type: GET_POST_FAILURE,
-            payload: e,
-            error: true
-        }); // 에러 발생
-        throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
-    }
-};
+// export const getPost = id => async dispatch => {
+//     dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+//     try {
+//         const response = await api.getPost(id);
+//         dispatch({
+//             type: GET_POST_SUCCESS,
+//             payload: response.data
+//         }); // 요청 성공
+//     } catch (e) {
+//         dispatch({
+//             type: GET_POST_FAILURE,
+//             payload: e,
+//             error: true
+//         }); // 에러 발생
+//         throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
+//     }
+// };
 
-export const getUsers = () => async dispatch => {
-    dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
-    try {
-        const response = await api.getUsers();
-        dispatch({
-            type: GET_USERS_SUCCESS,
-            payload: response.data
-        }); // 요청 성공
-    } catch (e) {
-        dispatch({
-            type: GET_USERS_FAILURE,
-            payload: e,
-            error: true
-        }); // 에러 발생
-        throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
-    }
-};
+// export const getUsers = () => async dispatch => {
+//     dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
+//     try {
+//         const response = await api.getUsers();
+//         dispatch({
+//             type: GET_USERS_SUCCESS,
+//             payload: response.data
+//         }); // 요청 성공
+//     } catch (e) {
+//         dispatch({
+//             type: GET_USERS_FAILURE,
+//             payload: e,
+//             error: true
+//         }); // 에러 발생
+//         throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
+//     }
+// };
+// 18.3.1.5 리팩토링
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태를 선언합니다.
 // 요청의 로딩 중 상태는 loading이라는 객체에서 관리합니다.
@@ -80,50 +85,50 @@ const initialState = {
 
 const sample = handleActions(
     {
-        [GET_POST]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: true // 요청 시작
-            }
-        }),
+        // [GET_POST]: state => ({
+        //     ...state,
+        //     loading: {
+        //         ...state.loading,
+        //         GET_POST: true // 요청 시작
+        //     }
+        // }),
         [GET_POST_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            },
+            // loading: {
+            //     ...state.loading,
+            //     GET_POST: false // 요청 완료
+            // },
             post: action.payload
         }),
-        [GET_POST_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_POST: false // 요청 완료
-            }
-        }),
-        [GET_USERS]: state => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: true // 요청 시작
-            }
-        }),
+        // [GET_POST_FAILURE]: (state, action) => ({
+        //     ...state,
+        //     loading: {
+        //         ...state.loading,
+        //         GET_POST: false // 요청 완료
+        //     }
+        // }),
+        // [GET_USERS]: state => ({
+        //     ...state,
+        //     loading: {
+        //         ...state.loading,
+        //         GET_USERS: true // 요청 시작
+        //     }
+        // }),
         [GET_USERS_SUCCESS]: (state, action) => ({
             ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            },
+            // loading: {
+            //     ...state.loading,
+            //     GET_USERS: false // 요청 완료
+            // },
             users: action.payload
-        }),
-        [GET_USERS_FAILURE]: (state, action) => ({
-            ...state,
-            loading: {
-                ...state.loading,
-                GET_USERS: false // 요청 완료
-            }
         })
+        // [GET_USERS_FAILURE]: (state, action) => ({
+        //     ...state,
+        //     loading: {
+        //         ...state.loading,
+        //         GET_USERS: false // 요청 완료
+        //     }
+        // })
     },
     initialState
 );
@@ -136,10 +141,13 @@ export default sample;
 import { combineReducers } from 'redux';
 import counter from './counter';
 import sample from './sample';
+// 18.3.1.5 리팩토링
+import loading from './loading';
 
 const rootReducer = combineReducers({
     counter,
-    sample
+    sample,
+    loading
 });
 
 export default rootReducer;
@@ -201,8 +209,20 @@ const SampleContainer = ({
 }) => {
     //클래스 형태 컴포넌트였다면 componentDidMount
     useEffect(() => {
-        getPost(1);
-        getUsers(1);
+        // 18.3.1.5 리팩토링
+        // getPost(1);
+        // getUsers(1);
+        // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에
+        // 그 내부에서 async 함수를 선언하고 호출해 줍니다.
+        const fn = async () => {
+            try {
+                await getPost(1);
+                await getUsers(1);
+            } catch (e) {
+                console.log(e); // 에러 조회
+            }
+        };
+        fn();
     }, [getPost, getUsers]);
     return (
         <Sample
@@ -214,12 +234,13 @@ const SampleContainer = ({
     );
 };
 
+// 18.3.1.5 리팩토링
 export default connect(
-    ({ sample }) => ({
+    ({ sample, loading }) => ({
         post: sample.post,
         users: sample.users,
-        loadingPost: sample.loading.GET_POST,
-        loadingUsers: sample.loading.GET_USERS
+        loadingPost: loading['sample/GET_POST'],
+        loadingUsers: loading['sample/GET_POST']
     }),
     {
         getPost,
@@ -242,3 +263,75 @@ return (
 };
 
 export default App;
+
+// 18.3.1.5 리팩토링
+/**
+ * lib/createRequestThunk.js
+ */
+import { startLoading, finishLoading } from '../modules/loading';
+
+export default function createRequestThunk(type, request) {
+// 성공 및 실패 액션 타입을 정의합니다.
+const SUCCESS = `${type}_SUCCESS`;
+const FAILURE = `${type}_FAILURE`;
+return params => async dispatch => {
+    dispatch({ type }); // 시작됩
+    try {
+        const response = await request(params);
+        dispatch({
+            type: SUCCESS,
+            payload: response.data
+        }); // 성공
+    } catch (e) {
+        dispatch({
+            type: FAILURE,
+            payload: e,
+            error: true
+        }); // 에러 발생
+        throw e;
+    }
+};
+}
+
+// 사용법: createRequestThunk('GET_USERS', api.getUsers);
+
+/**
+ * modules/loading.js
+ */
+import { createAction, handleActions } from 'redux-actions';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+/*
+요청을 위한 액션 타입을 payload로 설정합니다(예: "sample/GET_POST").
+*/
+
+export const startLoading = createAction(
+    START_LOADING,
+    requestType => requestType
+);
+
+export const finishLoading = createAction(
+    FINISH_LOADING,
+    requestType => requestType
+);
+
+const initialState = {};
+
+const loading = handleActions(
+    {
+        [START_LOADING]: (state, action) => ({
+            ...state,
+            [action.payload]: true
+        }),
+        [FINISH_LOADING]: (state, action) => ({
+            ...state,
+            [action.payload]: false
+        })
+    },
+    initialState
+);
+
+export default loading;
+
